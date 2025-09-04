c207ae92d73d61146d777c30bf89194e05ae2ea9
ojobointernational <91267354+ojobointernational@users.noreply.github.com>
2025-09-05 00:54:44 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
